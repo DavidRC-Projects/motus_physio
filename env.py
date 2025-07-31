71af46e5f1911dbd1623c573341ccfccd00670ac
David Carr <g3t2bqbb@students.codeinstitute.net>
2025-07-31 20:56:16 +0100

    Add packages to connect to PostgreSQL and Database url

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_qyo6kzAb3Zct@ep-floral-cloud-a2mfbqmv.eu-central-1.aws.neon.tech/drove_hedge_saved_895075")
+
+
+

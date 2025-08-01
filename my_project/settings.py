dcc38d334806eb7919da5676f50e57f45e715dd1
David Carr <g3t2bqbb@students.codeinstitute.net>
2025-08-01 21:11:07 +0100

    Add secret key to env.py file and update settings

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,6 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-f)@=)9n#riqlsa$@gfbg(fp&6ml57++&u5hu%ey^(((-mvldb2'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

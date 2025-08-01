2483e561d33568055fb2e8490dacf17158331c71
David Carr <g3t2bqbb@students.codeinstitute.net>
2025-08-01 21:11:07 +0100

    Add secret key to env.py file and update settings

--- env.py
+++ env.py
@@ -2,6 +2,5 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_qyo6kzAb3Zct@ep-floral-cloud-a2mfbqmv.eu-central-1.aws.neon.tech/drove_hedge_saved_895075")
-
-
+os.environ.setdefault("SECRET_KEY", "pY-%Bt4T1hawituhfsn!ajdfhf$!j")
 

6d934f84bd0636d5d9870b369138fb506038d03a
David Carr <g3t2bqbb@students.codeinstitute.net>
2025-08-02 12:36:14 +0100

    Add register app and register.html. Update settings and urls.py and add form

--- my_project/settings.py
+++ my_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-f)@=)9n#riqlsa$@gfbg(fp&6ml57++&u5hu%ey^(((-mvldb2
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'users',
+    'register',
 ]
 
 SITE_ID = 1

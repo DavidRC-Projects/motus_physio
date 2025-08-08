b400e2ade002efb8e983149e1227c49a0e5764ff
David Carr <g3t2bqbb@students.codeinstitute.net>
2025-08-08 17:30:01 +0100

    Add user profile page with name and email display

--- users/views.py
+++ users/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+@login_required
+def profile(request):
+    user = request.user
+    context = {
+        'user': user,
+    }
+    return render(request, 'users/profile.html', context)

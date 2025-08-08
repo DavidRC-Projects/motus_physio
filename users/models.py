15ba03daaf6c245d28ba28c1781965dc5c35e736
David Carr <g3t2bqbb@students.codeinstitute.net>
2025-08-08 16:53:33 +0100

    Add cloundinary storage

--- users/models.py
+++ users/models.py
@@ -1,16 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
-# Create your models here.
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
-    surgery_type = models.CharField(max_length=100, blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return f"{self.user.username}'s Profile"
 

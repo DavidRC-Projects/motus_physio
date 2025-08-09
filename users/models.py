54706d661400be3e5b6b1db9d80f960e35e48d55
David Carr <g3t2bqbb@students.codeinstitute.net>
2025-08-09 10:36:35 +0100

    Fix Cloudinary storage configuration. Add UserProfile Model with placeholder and surgery type field

--- users/models.py
+++ users/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = CloudinaryField('image', default='placeholder')
+    surgery_type = models.CharField(max_length=100, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
 
 

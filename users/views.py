1fa9e1b42c97a44a03a87a0cd6d0df4132ca107d
David Carr <g3t2bqbb@students.codeinstitute.net>
2025-08-10 15:51:10 +0100

    Update views.py and urls.py for bookings. Edit html in bookings.html

--- users/views.py
+++ users/views.py
@@ -9,6 +9,10 @@ import cloudinary.uploader
 def index(request):
     return render(request, 'index.html')
 
+@login_required
+def booking(request):
+    return render(request, 'users/booking.html')
+
 
 @login_required
 def profile(request):

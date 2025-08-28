dd7dfbcebcbf17fbc16f3906a86a368fe9938c98
David Carr <g3t2bqbb@students.codeinstitute.net>
2025-08-28 08:44:11 +0100

    Add class meta to testimonials model so testimonials are ordered from newest to oldest

--- users/models.py
+++ users/models.py
@@ -70,5 +70,8 @@ class Testimonials(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ['-created_at']
+
     def __str__(self):
         return f"Testimonial by {self.user.username}"

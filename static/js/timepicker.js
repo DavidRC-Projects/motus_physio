1f176f8dc623c535ef7cf528ced9194d1cecea8b
David Carr <g3t2bqbb@students.codeinstitute.net>
2025-08-30 14:14:08 +0100

    Remove images from TESTING.md

--- static/js/timepicker.js
+++ static/js/timepicker.js
@@ -1,3 +1,9 @@
+/* jshint jquery: true */
+/**
+ * Initializes timepicker on inputs with "timepicker" using jQuery.
+ * - 24-hour format, 30-min intervals, 09:00–21:00 range.
+ * - Dropdown enabled, dynamic updates disabled.
+ */
 $(document).ready(function() {
   $('input.timepicker').timepicker({
     timeFormat: 'H:i',

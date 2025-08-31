fd223530e25cfa01c0660f9c93529ded6093cc6f
David Carr <g3t2bqbb@students.codeinstitute.net>
2025-08-31 09:28:01 +0100

    Add steps for setup and deployment. Add Technologies Used in README.md

--- env.py
+++ env.py
@@ -3,4 +3,7 @@ import os
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_qyo6kzAb3Zct@ep-floral-cloud-a2mfbqmv.eu-central-1.aws.neon.tech/drove_hedge_saved_895075")
 os.environ.setdefault("SECRET_KEY", "pY-%Bt4T1hawituhfsn!ajdfhf$!j")
+os.environ.setdefault("CLOUDINARY_CLOUD_NAME", "dtyahkut6")
+os.environ.setdefault("CLOUDINARY_API_KEY", "382354952571716")
+os.environ.setdefault("CLOUDINARY_API_SECRET", "HGkNZfiV2Iwi0op-ClZZH4_x7j8")
 
